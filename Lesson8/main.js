b5d5fdb41604cfabe94fb1c3e5931afe11d27fc6
PashaAstrax <pashaastrax@gmail.com>
2024-08-27 14:36:23 +0200

    hw8 is done

--- Lesson8/main.js
+++ Lesson8/main.js
@@ -6,7 +6,5 @@ let coursesAndDurationArray = [
     {title: 'FullStack', monthDuration: 7},
     {title: 'Frontend', monthDuration: 4}
 ];
-
-// coursesAndDurationArray.map((item, index) => ({id: index + 1, ...item}));
 let mapCourse = coursesAndDurationArray.map((course, index) => ({ id: index + 1, ...course }));
 console.log(mapCourse)
